5b9194f5de765705255cdf19454e67d9fde74aad
Philippe Khill <khillbill@gmail.com>
2022-10-03 11:10:26 +0200

    namespace fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Galee\Casts\\Cast\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Galee\\Casts\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
